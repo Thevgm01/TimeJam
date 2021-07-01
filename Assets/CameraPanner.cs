c6ce8131e78b06be5d6e892e1976cc779c1a6ec6
Steven Dunn <thevgm01@gmail.com>
2021-06-30 21:58:00 -0500

    Added a basic mouse panning script

---
+++ Assets/CameraPanner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPanner : MonoBehaviour
+{
+    Vector3 velocity, desiredPosition;
+    public float lerpSpeed;
+    [Range(0, 1)] public float flingDecayStrength;
+
+    void Awake()
+    {
+        velocity = Vector3.zero;
+        desiredPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            velocity.x = -Input.GetAxis("Mouse X");
+            velocity.y = -Input.GetAxis("Mouse Y");
+        }
+        else if (velocity.sqrMagnitude > 0)
+        {
+            velocity *= 1 - flingDecayStrength;
+        }
+
+        desiredPosition += velocity;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, lerpSpeed * Time.deltaTime);
+    }
+}
