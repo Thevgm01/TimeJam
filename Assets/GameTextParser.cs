44fa304a0105b5a78c0b366446317f888a97dfc9
Steven Dunn <thevgm01@gmail.com>
2021-06-30 23:52:31 -0500

    Progress for the game text parser

---
+++ Assets/GameTextParser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTextParser : MonoBehaviour
+{
+    public TextAsset gameText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        string[] lines = gameText.text.Split('\n');
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            string line = lines[i].Trim();
+            if (line[0] == '[' && line[line.Length - 1] == ']')
+            {
+                ParseCommand(line);
+            }
+        }
+    }
+
+    private void ParseCommand(string line)
+    {
+
+    }
+}
