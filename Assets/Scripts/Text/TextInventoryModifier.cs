61961855cf39bc90be0d6e7c8fb8a93eea89b5ba
Steven Dunn <thevgm01@gmail.com>
2021-07-02 17:35:46 -0500

    Inventory modifiers are one class now

--- Assets/Scripts/Text/TextInventoryModifier.cs
+++ Assets/Scripts/Text/TextInventoryModifier.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class TextInventoryModifier : ITextInventory, ITextDisplayable
+public class TextInventoryModifier : ITextInventory, ITextDisplayable
 {
-    protected string item;
-    protected Inventory inventory;
+    public enum Operation
+    {
+        Add,
+        Remove
+    }
 
-    public abstract string GetText();
-    public abstract void ModifyInventory();
+    string item;
+    Inventory inventory;
+    Operation operation;
 
-    public TextInventoryModifier(Inventory inventory, string item)
+    public string GetText()
+    {
+        switch (operation)
+        {
+            case Operation.Add: return "You have gained " + item;
+            case Operation.Remove: return "You have lost " + item;
+        }
+        return "";
+    }
+
+    public void ModifyInventory()
+    {
+        switch (operation)
+        {
+            case Operation.Add: inventory.AddItem(item); return;
+            case Operation.Remove: inventory.RemoveItem(item); return;
+        }
+    }
+
+    public TextInventoryModifier(Inventory inventory, string item, Operation operation)
     {
         this.inventory = inventory;
         this.item = item;
+        this.operation = operation;
     }
 }
