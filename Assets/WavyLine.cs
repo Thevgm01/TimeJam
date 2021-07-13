2c0450696469d46f6d36217bd9fd5ee16d0c84c3
Thevgm01 <thevgm01@gmail.com>
2021-07-13 13:37:00 -0500

    Switch to Simplex Noise

--- Assets/WavyLine.cs
+++ Assets/WavyLine.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
-[ExecuteAlways]
 public class WavyLine : MonoBehaviour
 {
+    Noise noise;
+
     public Transform destination;
     public float resolutionPerUnit;
     float lastLength = 0;
 
-    public float scale = 10f;
+    public float amplitude = 10f;
+    public float frequency = 1f;
     public AnimationCurve noiseScaleOverLength;
 
     Vector3[] basePositions;
@@ -22,6 +24,8 @@ public class WavyLine : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        noise = new Noise();
+
         lr = GetComponent<LineRenderer>();
         seed = Random.Range(0f, 100000f);
     }
@@ -64,18 +68,17 @@ public class WavyLine : MonoBehaviour
             float frac = (float)i / (basePositions.Length - 1);
             float curveScale = noiseScaleOverLength.Evaluate(frac);
 
-            float curScale = scale;
             float time = Time.time;
 
             Vector3 point = basePositions[i];
-            float length = point.magnitude / scale;
+            float length = point.magnitude / frequency;
 
             Vector3 offset = new Vector3();
-            offset.x += Mathf.PerlinNoise(length - time - seed, seed);
-            offset.y += Mathf.PerlinNoise(seed, length - time - seed);
+            offset.x += noise.Evaluate(new Vector3(length - time - seed, seed, 0));
+            offset.y += noise.Evaluate(new Vector3(seed, length - time - seed, 0));
 
             offset = offset * 2 - new Vector3(1, 1, 0);
-            offset = offset * curScale * curveScale;
+            offset = offset * amplitude * curveScale;
             lr.SetPosition(i, point + offset);
         }
     }
