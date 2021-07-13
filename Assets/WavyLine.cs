37be73c560b63cf3ec51bbcb32fc4f807daca4e1
Thevgm01 <thevgm01@gmail.com>
2021-07-13 13:12:59 -0500

    Improved the wave appearance

--- Assets/WavyLine.cs
+++ Assets/WavyLine.cs
@@ -10,59 +10,73 @@ public class WavyLine : MonoBehaviour
     public float resolutionPerUnit;
     float lastLength = 0;
 
+    public float scale = 10f;
+    public AnimationCurve noiseScaleOverLength;
+
     Vector3[] basePositions;
 
     LineRenderer lr;
-    float scale = 10f;
-    float offset = 0f;
     float factor = 0.5f;
+    float seed;
 
     // Start is called before the first frame update
     void Awake()
     {
         lr = GetComponent<LineRenderer>();
+        seed = Random.Range(0f, 100000f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        SetPositions();
+        SetBasePositions();
         ApplyNoise();
     }
 
-    void SetPositions()
+    void SetBasePositions()
     {
         float length = (destination.position - transform.position).magnitude;
         if (length != lastLength)
         {
             int numPositions = Mathf.RoundToInt(length / resolutionPerUnit);
             basePositions = new Vector3[numPositions];
-            for (int i = 0; i < numPositions; ++i)
+            basePositions[0] = transform.position;
+            basePositions[numPositions - 1] = destination.position;
+
+            Vector3 diff = destination.position - transform.position;
+            Vector3 resolutionDiff = diff.normalized * resolutionPerUnit;
+            for (int i = 1; i < numPositions - 1; ++i)
             {
-                float frac = (float)i / (numPositions - 1);
-                basePositions[i] = Vector3.Lerp(transform.position, destination.position, frac);
+                basePositions[i] = basePositions[i - 1] + resolutionDiff;
             }
+
             lr.positionCount = numPositions;
             lr.SetPositions(basePositions);
+
             lastLength = length;
         }
     }
 
     void ApplyNoise()
     {
-        float curScale = scale;
         for (int i = 1; i < basePositions.Length - 1; i++)
         {
             float frac = (float)i / (basePositions.Length - 1);
-            Vector3 point = basePositions[i];
-            //for (int j = 0; j < 3; ++j)
-            //{
+            float curveScale = noiseScaleOverLength.Evaluate(frac);
 
-            //}
-            point.x += Mathf.PerlinNoise(point.x + Time.time, point.y);
-            point.y += Mathf.PerlinNoise(point.x, point.y + Time.time);
-            //scale *= factor;
-            lr.SetPosition(i, point);
+            float curScale = scale;
+            float time = Time.time;
+
+            Vector3 point = basePositions[i];
+            float length = point.magnitude / scale;
+
+            Vector3 offset = new Vector3();
+            offset.x += Mathf.PerlinNoise(length - time - seed, seed);
+            offset.y += Mathf.PerlinNoise(seed, length - time - seed);
+
+            offset = offset * 2 - new Vector3(1, 1, 0);
+            offset = offset * curScale * curveScale;
+            lr.SetPosition(i, point + offset);
         }
     }
 }
