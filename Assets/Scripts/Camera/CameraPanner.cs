8b906cf85dd15f503ccc944468bdc49d96fa9c01
Thevgm01 <thevgm01@gmail.com>
2021-07-14 15:58:33 -0500

    Can now fling the screen by dragging and releasing the mouse

--- Assets/Scripts/Camera/CameraPanner.cs
+++ Assets/Scripts/Camera/CameraPanner.cs
@@ -9,37 +9,57 @@ public class CameraPanner : MonoBehaviour, ICameraMover
     Vector3 cameraVelocity;
     Vector3 desiredCameraPosition;
 
+    struct MouseAtTime
+    {
+        public float time;
+        public Vector3 position;
+    }
+    Queue<MouseAtTime> mousePositions;
+    float secondsOfPositionsToHold = 0.05f;
+
     public float lerpSpeed;
-    [Range(0, 1)] public float velocityDecay;
+    public float slideSpeed = 0.1f;
+    [Range(0, 10)] public float velocityDecay;
 
     void Awake()
     {
         mainCam = Camera.main;
+        mousePositions = new Queue<MouseAtTime>();
     }
 
     void LateUpdate()
     {
         if (Input.GetMouseButton(0))
         {
-            cameraVelocity = mainCam.ScreenToWorldPoint(Input.mousePosition) - mainCam.ScreenToWorldPoint(lastMousePosition);
+            cameraVelocity = ScreenToWorldPointDifference(Input.mousePosition, lastMousePosition);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            cameraVelocity = ScreenToWorldPointDifference(Input.mousePosition, mousePositions.Peek().position);
+            cameraVelocity /= mousePositions.Count;
+        }
+        else
+        {
+            cameraVelocity -= cameraVelocity * Time.deltaTime * velocityDecay;
         }
 
         desiredCameraPosition -= cameraVelocity;
         transform.position = Vector3.Lerp(transform.position, desiredCameraPosition, lerpSpeed * Time.deltaTime);
 
-        lastMousePosition = Input.mousePosition;
-    }
+        while (mousePositions.Count > 2 && mousePositions.Peek().time < Time.time - secondsOfPositionsToHold)
+            mousePositions.Dequeue();
+        mousePositions.Enqueue(new MouseAtTime { time = Time.time, position = Input.mousePosition });
 
-    void FixedUpdate()
-    {
-        if (!Input.GetMouseButton(0))
-        {
-            cameraVelocity *= 1f - velocityDecay;
-        }
+        lastMousePosition = Input.mousePosition;
     }
 
     public void MoveToPoint(Vector3 point)
     {
         desiredCameraPosition = point;
+    }
+
+    Vector3 ScreenToWorldPointDifference(Vector3 screenPoint1, Vector3 screenPoint2)
+    {
+        return mainCam.ScreenToWorldPoint(screenPoint1) - mainCam.ScreenToWorldPoint(screenPoint2);
     }
 }
