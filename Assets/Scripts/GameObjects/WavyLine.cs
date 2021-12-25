e41a62412a04e6a20898b235a2634c7a5477cc62
Steven Dunn <thevgm01@gmail.com>
2021-12-24 22:18:07 -0600

    Made the wavy line look better when either end is moved

--- Assets/Scripts/GameObjects/WavyLine.cs
+++ Assets/Scripts/GameObjects/WavyLine.cs
@@ -3,25 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
-[ExecuteAlways]
 public class WavyLine : MonoBehaviour
 {
     Noise noise;
 
+    public Transform origin;
     public Transform destination;
-    [Range(0, 1)] public float resolutionPerUnit;
+    [Range(0.0f, 3.0f)] public float resolution;
     float lastLength = 0;
 
     [Header("Noise")]
     [Range(0, 30)] public float amplitude = 10f;
-    [Range(0, 0.2f)] public float frequency = 1f;
+    [Range(0, 1.0f)] public float frequency = 1f;
     [Range(-3, 3)] public float speed = 1f;
     [Range(-3, 3)] public float textureSpeed = 1f;
     public AnimationCurve noiseScaleOverLength;
     float noiseTimeTracker = 0;
 
     Vector3[] basePositions;
-    float[] lengths;
     float[] curveScales;
 
     LineRenderer lr;
@@ -39,42 +38,37 @@ public class WavyLine : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SetBasePositions();
-        ApplyNoise();
-        ScrollTexture();
-    }
-
-    void SetBasePositions()
-    {
-        float length = (destination.position - transform.position).magnitude;
+        float length = Vector3.Distance(origin.position, destination.position);
         if (length != lastLength)
         {
-            int numPositions = Mathf.RoundToInt(length / resolutionPerUnit);
-            basePositions = new Vector3[numPositions];
-            lengths = new float[numPositions];
-            curveScales = new float[numPositions];
+            SetBasePositions(length);
+        }
+        ApplyNoise();
+        ScrollTexture(length);
+        lastLength = length;
+    }
 
-            basePositions[0] = Vector3.zero;
-            basePositions[numPositions - 1] = destination.position - transform.position;
-            Vector3 diff = destination.position - transform.position;
-            Vector3 resolutionDiff = diff / numPositions;
+    void SetBasePositions(float length)
+    {
+        int numPositions = Mathf.CeilToInt(length * resolution);
 
-            lr.positionCount = numPositions;
-            lr.SetPosition(0, basePositions[0]);
-            lr.SetPosition(numPositions - 1, basePositions[numPositions - 1]);
+        basePositions = new Vector3[numPositions];
+        basePositions[0] = origin.position;
+        basePositions[numPositions - 1] = destination.position;
 
-            for (int i = 1; i < numPositions - 1; ++i)
-            {
-                basePositions[i] = basePositions[i - 1] + resolutionDiff;
+        curveScales = new float[numPositions];
+        curveScales[0] = 0.0f;
+        curveScales[numPositions - 1] = 0.0f;
 
-                lengths[i] = basePositions[i].magnitude;
+        lr.positionCount = numPositions;
+        lr.SetPosition(0, origin.position);
+        lr.SetPosition(numPositions - 1, destination.position);
 
-                float frac = (float)i / (basePositions.Length - 1);
-                float curveScale = noiseScaleOverLength.Evaluate(frac);
-                curveScales[i] = curveScale;
-            }
-
-            lastLength = length;
+        for (int i = 1; i < numPositions - 1; ++i)
+        {
+            float frac = (float)i / numPositions;
+            basePositions[i] = Vector3.Lerp(origin.position, destination.position, frac);
+            curveScales[i] = noiseScaleOverLength.Evaluate(frac);
         }
     }
 
@@ -83,27 +77,22 @@ public class WavyLine : MonoBehaviour
         if (!lr.isVisible)
             return;
 
-        if (noise == null)
-            noise = new Noise();
-
         noiseTimeTracker += Time.deltaTime * speed;
         for (int i = 1; i < basePositions.Length - 1; i++)
         {
-            Vector3 point = basePositions[i];
+            float frac = (float)i / basePositions.Length;
+
             Vector3 offset = new Vector3();
-
-            float length = lengths[i] * frequency;
-
-            offset.x += noise.Evaluate(length + noiseTimeTracker, 0);
-            offset.y += noise.Evaluate(0, length + noiseTimeTracker);
-
+            offset.x += noise.Evaluate(frac * frequency + noiseTimeTracker, 0);
+            offset.y += noise.Evaluate(0, frac * frequency + noiseTimeTracker);
             offset = offset * amplitude * curveScales[i];
-            lr.SetPosition(i, point + offset);
+
+            lr.SetPosition(i, basePositions[i] + offset);
         }
     }
 
-    void ScrollTexture()
+    void ScrollTexture(float length)
     {
-        lr.sharedMaterial.mainTextureOffset += Vector2.right * textureSpeed * Time.deltaTime;
+        lr.material.mainTextureOffset += Vector2.right * (textureSpeed * Time.deltaTime + (lastLength - length) * lr.material.mainTextureScale.x / 2.0f);
     }
 }
